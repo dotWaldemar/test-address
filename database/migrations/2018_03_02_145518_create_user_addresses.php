e8578ecdd0c8ed38585f376a2fa4deda642a973e
dotWaldemar <waldemar.dassler@gmail.com>
2018-03-02 17:09:57 +0200

    Address migration created

--- database/migrations/2018_03_02_145518_create_user_addresses.php
+++ database/migrations/2018_03_02_145518_create_user_addresses.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePasswordResetsTable extends Migration
+class CreateUserAddresses extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,14 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
+        Schema::create('address', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('address_name');
+            $table->string('stret');
+            $table->string('city');
+            $table->string('area');
+            $table->string('house');
+            $table->text('additional_information');
             $table->timestamp('created_at')->nullable();
         });
     }
@@ -27,6 +32,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        //
     }
 }
